dcad34ee07e64e77f9973495f6ecb6a5167256b3
HenriqueGon <henriquegonformento@gmail.com>
2025-11-26 20:43:18 -0300

    Delete button.py

--- button.py
+++
@@ -1,8 +0,0 @@
-import pygame
-class Button(pygame.Rect):
-    def __init__(self, x, y, length, width, cor):
-        super().__init__(x, y, length, width)
-        self.cor = cor
-        
-    def press(self):
-        pass
