5c766a9b52ea585816f3819bb54c519d349b0dcc
HenriqueGon <henriquegonformento@gmail.com>
2025-11-26 20:43:03 -0300

    Delete b_menos.py

--- b_menos.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from button import Button
-
-class B_Menos(Button):
-    def __init__(self):
-        super().__init__(350, 700, 75, 75, (0, 255, 0))
-        
-    def press(self):
-        print("Reduzir a aposta")
